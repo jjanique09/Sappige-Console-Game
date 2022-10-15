5e173dbc5d04f8b85074d5b2ac513eaaac10a858
jjanique09 <jjap.vandenoever@student.avans.nl>
2022-10-15 02:11:46 +0200

    Jason reader uitbreiding

--- Level Data/JsonReader.cs
+++ Level Data/JsonReader.cs
@@ -5,45 +5,95 @@ namespace Level_Data
 {
     class JsonReader : IStrategy
     {
-        const string JsonPath = @"../LevelDataJson.json";
-        JObject GameJsonObj = JObject.Parse(File.ReadAllText(JsonPath));
-
+        JObject GameJsonObj = JObject.Parse(File.ReadAllText(@"../LevelDataJson.json"));
 
         public object DoAlgorithm(object data)
         {
-
-
-
-
-
+            Console.WriteLine("Test " + CreatePlayer().startX);
             var list = data as List<string>;
             list.Sort();
-
             return list;
         }
 
-    /*   public Player CreatePlayer()
-         {
-            string a = 
-
-            var player = json["player"];
-            var startRoomId = player["startRoomId"].Value<int>();
-            var x = player["startX"].Value<int>();
-            var y = player["startY"].Value<int>();
-            var lives = player["lives"].Value<int>();
-        
-            return new Player(startRoomId, startPosition, player["lives"].Value<int>());
-        }*/
-
-
-        public class Player
+        public Player CreatePlayer()
         {
+            JToken player = GameJsonObj["player"];
+            return new Player
+            {
+                startY = player["startY"].Value<int>(),
+                startX = player["startX"].Value<int>(),
+                startRoomId = player["startRoomId"].Value<int>(),
+                lives = player["lives"].Value<int>(),
+            };   
+        }
 
+        public List<Room> CreateRoom()
+        {
+            List<Room> roomList = new List<Room>();
+            foreach (var room in GameJsonObj["rooms"])
+            {
+                List<Item> setItems = new List<Item>();
+                if (room["items"] != null) { setItems = CreateRoomItems(JToken.FromObject(room["items"])); }
+                roomList.Add(new Room
+                {
+                    id = room["id"].Value<int>(),
+                    width = room["width"].Value<int>(),
+                    height = room["height"].Value<int>(),
+                    type = room["type"].Value<string>(),
+                    items = setItems
+                });
+            }
+            return roomList;
+        }
+
+        private List<Item> CreateRoomItems(JToken items)
+        {
+            List<Item> itemList = new List<Item>();
+            foreach (var jsonItem in items)
+            {
+                itemList.Add(new Item
+                 {
+                    type = jsonItem["type"].Value<string>(),
+                    color = (jsonItem["color"] ?? "").Value<string>(),
+                    damage = (jsonItem["int"] ?? "").Value<int>(),
+                    x = jsonItem["x"].Value<int>(),
+                    y = jsonItem["y"].Value<int>(),
+            });
+            }
+            return itemList;
+        }
+
+
+        public class Item 
+        {
+           public string type { get; set; }
+           public string color { get; set; }
+           public int x { get; set; }
+           public int y { get; set; }
+           public int damage { get; set; }
+        }
+
+
+
+        public class Room
+        {
+            public int id { get; set; }
+            public int width { get; set; }
+            public int height { get; set; }
+            public string type { get; set; }
+            public List<Item> items { get; set; }
+        }
+
+
+
+
+
+public class Player
+        {
             public int startRoomId { get; set; }
             public int startX { get; set; }
             public int startY { get; set; }
             public int lives { get; set; }
-
         }
 	}
 }
