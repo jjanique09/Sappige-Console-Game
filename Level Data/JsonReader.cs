ce1b4d317708aad4b786a27fa7cdfe3afa70485b
jjanique09 <jjap.vandenoever@student.avans.nl>
2022-11-09 22:11:57 +0100

    Using The Decprator Pattern

--- Level Data/JsonReader.cs
+++ Level Data/JsonReader.cs
@@ -7,56 +7,49 @@ namespace Level_Data
 {
     class JsonReader : IStrategy
     {
-        JObject GameJsonObj = JObject.Parse(File.ReadAllText(@"../LevelDataJson.json"));
 
+        JObject GameJsonObj = JObject.Parse(File.ReadAllText(@"../LevelDataJson.json"));
         public object DoAlgorithm(object data)
         {
-
-            PlainDoor plainPizzaObj = new PlainDoor();
-            DoorDecorator chickenPizzaDecorator = new ColoredDoorDecorator(plainPizzaObj);
-            Door vegPizzaDecorator = new VegPizzaDecorator(chickenPizzaDecorator);
-
-            Console.WriteLine("1 "+ chickenPizzaDecorator.Open());
-            Console.WriteLine("1 "+ vegPizzaDecorator.Open());
-
-            Console.Read();
-
-            /*
-
-            Console.ForegroundColor = ConsoleColor.Green;
-                        Console.WriteLine("\nPrinted Player\n" + " Lives = " + CreatePlayer().lives + "\n StartX = " + CreatePlayer().startX + "\n StartY = " + CreatePlayer().startY + "\n StartRoomId = " + CreatePlayer().startRoomId);
-                        Console.ForegroundColor = ConsoleColor.Blue;
-                        Console.WriteLine("\nPrinting All Rooms");
-                        foreach (var room in CreateRoom())
-                        {
-                            Console.WriteLine(" RoomID = " + room.id + "\n Height = " + room.height + "\n Width = " + room.width + "\n Type = " + room.type + "\n");
-                            foreach (var roomItem in room.items)
-                            {
-                                Console.ForegroundColor = ConsoleColor.Magenta;
-                                Console.WriteLine("  Item Type = " + roomItem.type + "\n" + "  Color = " + roomItem.color + "\n" + "  X = " + roomItem.x + "\n" + "  Y = " + roomItem.y + "\n" + "  Damage = " + roomItem.damage + "\n");
-                            }
-                            Console.ForegroundColor = ConsoleColor.Blue;
-                        }
-                        Console.ForegroundColor = ConsoleColor.White;
-            */
-
-
+            List<Connection> connections = CreateConnection(GameJsonObj);
             var list = data as List<string>;
             list.Sort();
             return list;
         }
+
+
+        public void PrintLoadedGameData()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("\nPrinted Player\n" + " Lives = " + CreatePlayer().lives + "\n StartX = " + CreatePlayer().startX + "\n StartY = " + CreatePlayer().startY + "\n StartRoomId = " + CreatePlayer().startRoomId);
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("\nPrinting All Rooms");
+            foreach (var room in CreateRoom())
+            {
+                Console.WriteLine(" RoomID = " + room.id + "\n Height = " + room.height + "\n Width = " + room.width + "\n Type = " + room.type + "\n");
+                foreach (var roomItem in room.items)
+                {
+                    Console.ForegroundColor = ConsoleColor.Magenta;
+                    Console.WriteLine("  Item Type = " + roomItem.type + "\n" + "  Color = " + roomItem.color + "\n" + "  X = " + roomItem.x + "\n" + "  Y = " + roomItem.y + "\n" + "  Damage = " + roomItem.damage + "\n");
+                }
+                Console.ForegroundColor = ConsoleColor.Blue;
+            }
+
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
 
         public interface Door
         {
             bool Open(Game game);
         }
 
-        
+
         public class PlainDoor : Door
         {
             public bool Open(Game game)
             {
-               return true;
+                return true;
             }
         }
 
@@ -76,6 +69,7 @@ namespace Level_Data
             }
         }
 
+
         public class ColoredDoorDecorator : DoorDecorator
         {
             public ColoredDoorDecorator(Door door) : base(door)
@@ -85,7 +79,7 @@ namespace Level_Data
             {
                 if (door.Open(game) == true)
                 {
-                    if (game.player.items.FindAll(x=>x.type.Equals("key") && x.color.Equals("red")).Count() > 0 ) 
+                    if (game.player.items.FindAll(x => x.type.Equals("key") && x.color.Equals("red")).Count() > 0)
                     {
                         return true;
                     }
@@ -95,7 +89,6 @@ namespace Level_Data
             }
         }
 
-        // Stap 5 Creating Veg Pizza Decorator
 
         public class VegPizzaDecorator : DoorDecorator
         {
@@ -106,7 +99,7 @@ namespace Level_Data
             {
                 if (door.Open(game) == true)
                 {
-                    if (player == null)
+                    if (game.player.items.FindAll(x => x.type.Equals("key") && x.color.Equals("red")).Count() > 0)
                     {
                         return true;
                     }
@@ -117,35 +110,6 @@ namespace Level_Data
         }
 
 
-
-
-
-
-
-        // Je wil hebt een methode die bepaald of een deur open mag voor elk verschillend deur type - Fixed
-        // Deze methode geeft een booelean terug maar hoe decorate je dan 2 verschillende deuren?
-        // Er moeten parameters worden doorgevoerd om bepaalde variabelen te setten zoals: color, no_of_stones. het variabele "Type" word bepaald door de decorator
-        // 
-
-
-
-
-
-
-
-
-        /*
-                public class Door
-                {
-                    public string? type { get; set; }
-                    public string? color { get; set; }
-                    public int? no_of_stones { get; set; }
-
-                }
-
-
-        */
-
         public Player CreatePlayer()
         {
             JToken player = GameJsonObj["player"];
@@ -155,22 +119,24 @@ namespace Level_Data
                 startX = player["startX"].Value<int>(),
                 startRoomId = player["startRoomId"].Value<int>(),
                 lives = player["lives"].Value<int>(),
-            };   
+            };
         }
+
+
         public List<Room> CreateRoom()
         {
             List<Room> roomList = new List<Room>();
             foreach (var room in GameJsonObj["rooms"])
             {
                 List<Item> setItems = new List<Item>();
-                if (room["items"] != null) { setItems = CreateRoomItems(JToken.FromObject(room["items"])); } 
+                if (room["items"] != null) { setItems = CreateRoomItems(JToken.FromObject(room["items"])); }
                 roomList.Add(new Room
                 {
                     id = room["id"].Value<int>(),
                     width = room["width"].Value<int>(),
                     height = room["height"].Value<int>(),
                     type = room["type"].Value<string>(),
-                    items = setItems 
+                    items = setItems
                 });
             }
             return roomList;
@@ -184,11 +150,11 @@ namespace Level_Data
             {
                 itemList.Add(new Item
                 {
-                type = jsonItem["type"].Value<string>(),
-                color = (jsonItem["color"] ?? "").Value<string>(),
-                x = jsonItem["x"].Value<int>(),
-                y = jsonItem["y"].Value<int>(),
-                damage = (jsonItem["damage"] ?? 0).Value<int>()
+                    type = jsonItem["type"].Value<string>(),
+                    color = (jsonItem["color"] ?? "").Value<string>(),
+                    x = jsonItem["x"].Value<int>(),
+                    y = jsonItem["y"].Value<int>(),
+                    damage = (jsonItem["damage"] ?? 0).Value<int>()
                 });
             }
             return itemList;
@@ -205,29 +171,73 @@ namespace Level_Data
                 if (connection["EAST"] != null) { setConnection.east = connection["EAST"].Value<int?>(); }
                 if (connection["WEST"] != null) { setConnection.west = connection["WEST"].Value<int?>(); }
                 if (connection["SOUTH"] != null) { setConnection.south = connection["SOUTH"].Value<int?>(); }
-                
+
                 List<Door> doors = new List<Door>();
-                if (connection["doors"] != null) { setConnection.doors = CreateDoors(JToken.FromObject(connection["doors"])); }
+                if (connection["doors"] != null) { setConnection.door = CreateDoors(JToken.FromObject(connection["doors"])); }
+               // if (connection["doors"] != null) { CreateDoors(JToken.FromObject(connection["doors"])); }
                 connections.Add(setConnection);
             }
             return connections;
         }
 
 
-        private Door CreateDoors(JToken items)
+        private Door CreateDoors(JToken doorToken)
+        {
+            bool firstInitilizer = true;
+            DoorDecorator? doorDecorator = null;
+            Console.WriteLine("");
+            foreach (var jsonItem in doorToken)
+            {
+
+                if (firstInitilizer == true)
+                {
+                    PlainDoor doorObj = new PlainDoor();
+                    string type = jsonItem["type"].Value<string>();
+                    if (type.Equals("colored")) { doorDecorator = new ColoredDoorDecorator(doorObj); Console.Write("You Decorated Colors with"); }
+                    if (type.Equals("open on odd")) { doorDecorator = new VegPizzaDecorator(doorObj); Console.Write("You Decorated Open On Odd with"); }
+
+                    //Uncomment check if continuity is still correct ---> // Console.WriteLine("Dit moet een 1 zijn verdikkie " + doorToken.Count());
+                    //Console.WriteLine(doorToken.Count());
+                    firstInitilizer = false;
+                    if (doorToken.Count() == 1) { return doorDecorator;} // fixes the one issue but 2's still run twice
+                }
+                else
+                {
+                    //Uncomment check if continuity is still correct ---> // Console.WriteLine("Dit mag geen 1 zijn verdikkie " + doorToken.Count());
+                    Door? xDecorator = null;
+                    string type = jsonItem["type"].Value<string>();
+                    if (type.Equals("colored")) { xDecorator = new ColoredDoorDecorator(doorDecorator); Console.Write("Colored"); }
+                    if (type.Equals("open on odd")) { xDecorator = new VegPizzaDecorator(doorDecorator); Console.Write(" Open On Odd"); } 
+                    
+                    return xDecorator;
+                }
+            }
+            return doorDecorator;
+        }
+           
+
+
+
+        /// Old Code
+   /*     private Door CreateDoors(JToken items)
         {
             Door itemList = new Door();
             foreach (var jsonItem in items)
             {
                 var type = jsonItem["type"].Value<string>();
-                Door a = new Door();
+                //Door a = new Door();
+
+
+                PlainDoor plainPizzaObj = new PlainDoor();
+                DoorDecorator chickenPizzaDecorator = new ColoredDoorDecorator(plainPizzaObj);
+                Door vegPizzaDecorator = new VegPizzaDecorator(chickenPizzaDecorator);
+
+                Console.WriteLine("1 " + chickenPizzaDecorator.Open());
+                Console.WriteLine("1 " + vegPizzaDecorator.Open());
+
             }
             return itemList;
-        }
-
-
-
-
+        }*/
 
 
 
@@ -239,6 +249,10 @@ namespace Level_Data
         /// 
         /// </summary>
 
+
+
+
+        
         public class Game
         {
             public Game(Player setPlayer, List<Connection> setConnections, List<Room> setRooms)
@@ -251,9 +265,7 @@ namespace Level_Data
             public Player player { get; set; }
             public List<Connection> connections { get; set; }
             public List<Room> rooms { get; set; } 
-            
         }
-
 
         public class Connection
         {
@@ -261,7 +273,7 @@ namespace Level_Data
             public int? east { get; set; }
             public int? south { get; set; }
             public int? west { get; set; }
-            public List<Door>? doors { get; set; }
+            public Door door { get; set; }
         }
 
 
@@ -292,8 +304,6 @@ namespace Level_Data
             public int startY { get; set; }
             public int lives { get; set; }
             public List<Item> items { get; set; }
-
-
         }
     }
 }
--- User Interface/Class1.cs
+++ User Interface/Class1.cs
@@ -3,3 +3,35 @@
     {
 
     }
+
+
+
+
+
+
+
+/// <ToDo>
+///Je wil hebt een methode die bepaald of een deur open mag voor elk verschillend deur type - Fixed
+/// Deze methode geeft een booelean terug maar hoe decorate je dan 2 verschillende deuren? - Fixed
+/// Er moeten parameters worden doorgevoerd om bepaalde variabelen te setten zoals: color, no_of_stones.
+/// het variabele "Type" word bepaald door de decorator
+/// </ToDo>   private Door CreateDoors(JToken items)
+/// 
+/*{
+    Door itemList = new Door();
+    foreach (var jsonItem in items)
+    {
+        var type = jsonItem["type"].Value<string>();
+        Door a = new Door();
+
+
+        PlainDoor plainPizzaObj = new PlainDoor();
+        DoorDecorator chickenPizzaDecorator = new ColoredDoorDecorator(plainPizzaObj);
+        Door vegPizzaDecorator = new VegPizzaDecorator(chickenPizzaDecorator);
+
+        Console.WriteLine("1 " + chickenPizzaDecorator.Open());
+        Console.WriteLine("1 " + vegPizzaDecorator.Open());
+
+    }
+    return itemList;
+}*/
