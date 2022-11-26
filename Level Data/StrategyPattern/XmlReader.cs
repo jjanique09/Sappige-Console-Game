41ff0fec92e2c8456939dfbe5ee5fe091e601f40
jjanique09 <jjap.vandenoever@student.avans.nl>
2022-11-26 13:27:55 +0100

    Cleanup xmlreader

--- Level Data/StrategyPattern/XmlReader.cs
+++ Level Data/StrategyPattern/XmlReader.cs
@@ -10,20 +10,13 @@ namespace Level_Data.StrategyPattern
 
         public XmlReader()
         {
-            //https://stackoverflow.com/questions/642293/how-do-i-read-and-parse-an-xml-file-in-c
+            // https://stackoverflow.com/questions/642293/how-do-i-read-and-parse-an-xml-file-in-c
+            // https://stackoverflow.com/questions/13605396/c-sharp-get-xml-tag-value
 
             XmlDocument doc = new XmlDocument();
             doc.Load(@"../LevelDataXml.xml");
-
-            XmlNode node = doc.DocumentElement.SelectSingleNode("/temple");
-            //var RoomId = node.ChildNodes.OfType<XmlElement>().Where(e => e.LocalName == "player lives").First().InnerText;
-
-
-            // https://stackoverflow.com/questions/13605396/c-sharp-get-xml-tag-value
-            
-            
-            Console.WriteLine(node.FirstChild);
         }
+
 
         public Game CreateBasedOnFile()
         {
@@ -34,18 +27,17 @@ namespace Level_Data.StrategyPattern
         public Player CreatePlayer(XmlDocument doc)
         {
 
-            XmlNode node = doc.DocumentElement.SelectSingleNode("/temple/player/start");
-            XmlNode templeNode = doc.DocumentElement.SelectSingleNode("/temple/player");
+            XmlNode playerNode = doc.DocumentElement.SelectSingleNode("/temple/player/start");
 
             return new Player
             {
-                startY = Int32.Parse(node.ChildNodes.OfType<XmlElement>().Where(e => e.LocalName == "y").First().InnerText),
-                startX = Int32.Parse(node.ChildNodes.OfType<XmlElement>().Where(e => e.LocalName == "x").First().InnerText),
-                startRoomId = Int32.Parse(node.ChildNodes.OfType<XmlElement>().Where(e => e.LocalName == "roomId").First().InnerText),
+                startY = Int32.Parse(playerNode.ChildNodes.OfType<XmlElement>().Where(e => e.LocalName == "y").First().InnerText),
+                startX = Int32.Parse(playerNode.ChildNodes.OfType<XmlElement>().Where(e => e.LocalName == "x").First().InnerText),
+                startRoomId = Int32.Parse(playerNode.ChildNodes.OfType<XmlElement>().Where(e => e.LocalName == "roomId").First().InnerText),
                 lives = Int32.Parse(doc.GetElementsByTagName("player")[0].Attributes["lives"].Value),
-
-        };
+            };
         }
+
     }
 }
 
